00114c09102ec1def05ea1a9c5e05a65291626a3
mercaderd <mercaderd@yahoo.es>
2020-04-15 22:11:35 +0200

    OTro mas

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ app.layout = html.Div(children=[
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run_server(port=port)
+    app.run_server(host='0.0.0.0', port=port)
