4cbd9a5c171afeaf3ec551856307fc74d1dc2ef0
mercaderd <mercaderd@yahoo.es>
2020-04-16 17:26:06 +0200

    First release

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ df_global_deaths = pd.read_csv('./deaths_data.csv', index_col=0)
 df_eu_confirmed = pd.read_csv('./confirmed_eu_data.csv', index_col=0)
 df_eu_deaths = pd.read_csv('./deaths_eu_data.csv', index_col=0)
 
-tr_global_confirmed = [go.Scatter(x=df_global_confirmed.index, y=df_global_confirmed[s].values, name=s, mode='lines+markers') for s in df_global_confirmed.columns]
-tr_global_deaths = [go.Scatter(x=df_global_deaths.index, y=df_global_deaths[s].values, name=s, mode='lines+markers') for s in df_global_deaths.columns]
-tr_eu_confirmed = [go.Scatter(x=df_eu_confirmed.index, y=df_eu_confirmed[s].values, name=s, mode='lines+markers') for s in df_eu_confirmed.columns]
-tr_eu_deaths = [go.Scatter(x=df_eu_deaths.index, y=df_eu_deaths[s].values, name=s, mode='lines+markers') for s in df_eu_deaths.columns]
+tr_global_confirmed = [go.Scatter(x=df_global_confirmed.index, y=df_global_confirmed[s].values, name=s, mode='lines') for s in df_global_confirmed.columns]
+tr_global_deaths = [go.Scatter(x=df_global_deaths.index, y=df_global_deaths[s].values, name=s, mode='lines') for s in df_global_deaths.columns]
+tr_eu_confirmed = [go.Scatter(x=df_eu_confirmed.index, y=df_eu_confirmed[s].values, name=s, mode='lines') for s in df_eu_confirmed.columns]
+tr_eu_deaths = [go.Scatter(x=df_eu_deaths.index, y=df_eu_deaths[s].values, name=s, mode='lines') for s in df_eu_deaths.columns]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -27,54 +27,66 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Daily updated Covid-19 data
     '''),
+    html.Table(children=[
+        html.Tr(children=[
+            html.Td(children=[
+                dcc.Graph(
+                        id='global-confirmed',
+                        figure={
+                            'data': tr_global_confirmed,
+                            'layout': {
+                                'title': 'Covid-19 EU-USA Confirmed',
+                                'yaxis': {'type': 'log'}
 
-    dcc.Graph(
-        id='global-confirmed',
-        figure={
-            'data': tr_global_confirmed,
-            'layout': {
-                'title': 'Covid-19 EU-USA Confirmed',
-                'yaxis': {'type': 'log'}
+                            }
+                        }
+                    )
+            ], style={"height" : "40%", "width" : "40%"}),
+            html.Td(children=[
+                dcc.Graph(
+                        id='global-deaths',
+                        figure={
+                            'data': tr_global_deaths,
+                            'layout': {
+                                'title': 'Covid-19 EU-USA Deaths',
+                                'yaxis': {'type': 'log'}
 
-            }
-        }
-    ),
+                            }
+                        }
+                    )
+            ], style={"height" : "40%", "width" : "40%"})
 
-dcc.Graph(
-        id='global-deaths',
-        figure={
-            'data': tr_global_deaths,
-            'layout': {
-                'title': 'Covid-19 EU-USA Deaths',
-                'yaxis': {'type': 'log'}
+        ]),
+        html.Tr(children=[
+            html.Td(children=[
+                dcc.Graph(
+                        id='eu-confirmed',
+                        figure={
+                            'data': tr_eu_confirmed,
+                            'layout': {
+                                'title': 'Covid-19 EU Confirmed',
+                                'yaxis': {'type': 'log'}
 
-            }
-        }
-    ),
+                            }
+                        }
+                    )
+            ], style={"height" : "40%", "width" : "40%"}),
+            html.Td(children=[
+                dcc.Graph(
+                        id='eu-deaths',
+                        figure={
+                            'data': tr_eu_deaths,
+                            'layout': {
+                                'title': 'Covid-19 EU Deaths',
+                                'yaxis': {'type': 'log'}
 
-dcc.Graph(
-        id='eu-confirmed',
-        figure={
-            'data': tr_eu_confirmed,
-            'layout': {
-                'title': 'Covid-19 EU Confirmed',
-                'yaxis': {'type': 'log'}
+                            }
+                        }
+                    )
+            ], style={"height" : "40%", "width" : "40%"})
+        ])
 
-            }
-        }
-    ),
-
-dcc.Graph(
-        id='eu-deaths',
-        figure={
-            'data': tr_eu_deaths,
-            'layout': {
-                'title': 'Covid-19 EU Deaths',
-                'yaxis': {'type': 'log'}
-
-            }
-        }
-    )
+    ])
 ])
 
 
